5a7c3d9de43488cfbfb32911b8804ddb7e750908
Holeekey <114121356+Holeekey@users.noreply.github.com>
2023-01-13 13:06:01 -0400

    userprocess commented

--- userprocess/src/main.rs
+++ userprocess/src/main.rs
@@ -1,5 +1,6 @@
 use std::process::Command;
 
+//Función que ejecuta el comando, junto con sus argumentos y duvuelve el resultado de la ejecución 
 fn execute_command(command: String, args: Vec<String>) -> String {
     let output = Command::new(command).args(args).output().unwrap();
     let value = String::from_utf8(output.stdout).unwrap();
@@ -7,6 +8,8 @@ fn execute_command(command: String, args: Vec<String>) -> String {
 }
 
 fn main() {
+//Se almacena en "data" el mayor número de procesos activos que puede tener un usuario a la vez
     let data = execute_command(String::from("bash"), vec![String::from("-c"), String::from("ulimit -u")]);
+//Se imprime "data"   
     println!("Num max Pid: {}", data);
 }
